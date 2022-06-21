265c93ee5b8725900c133e9bb53775b04863496c
Oded Levy <odedlevy02@gmail.com>
2022-06-21 13:25:19 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # node-integration-test-sample
+
+To build and execute this sample:
+1. cd to 'game-service' and run:
+```
+//install packages
+npm i
+//compile typescript
+tsc
+//run the project
+node .
+```
+2. Do the same for 'players-service'
+3. In the logs you will see a link to swagger. Open it and start testing the apis
+4. For running the tests in debug mode just (in VSCODE) just open 'RUN AND DEBUG' and select  'player-serve-tests' and run them
--- players-service/test/roomsBuilderTest.ts
+++ players-service/test/roomsBuilderTest.ts
@@ -6,24 +6,28 @@ import { PlayerEntity } from "../entities/player.entity";
 import { initDbStoreForTests } from "../dal/dbConnectionManager";
 import { RoomBuilder } from "../managers/roomBuilder";
 import { createPlayers, onGetGamesReturn } from "./roomsTestsHelper";
+import { RoomsService } from "../routes/rooms/rooms.service";
 
 describe("roomsBuilder tests", () => {
     beforeEach(async ()=>{
         await initDbStoreForTests()
     })
-    it("should map 5 player to 2 rooms", async () => {
+    it("should map 6 player to 2 rooms", async () => {
         const games: IGame[] = [
             { id: 1, name: "game1" },
             { id: 2, name: "game2" }
         ]
         onGetGamesReturn(games)
         const playerService = new PlayersService();
-        await createPlayers(5,1)
+        await createPlayers(6,1)
         await new RoomBuilder().mapPlayersInRooms()
         //get the players and validate that they are mapped to 2 rooms
         const players = await playerService.getAllPlayers(null)
-        assert(players[0].roomId==1)
-        assert(players[4].roomId==2)
+        const playersInRoom1 = players.filter(player=>player.roomId==1)
+        const playersInRoom2 = players.filter(player=>player.roomId==2)
+        assert(playersInRoom1.length==4)
+        assert(playersInRoom2.length==2)
+
     })
 
 });
